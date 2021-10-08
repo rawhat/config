8078482dbf892a5ee91675f003fc56c53d0ac28d
Alex Manning <alex41290@gmail.com>
2021-10-07 20:09:58 -0400

    support multi select on enter for telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,29 @@
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+function fzf_multi_select(prompt_bufnr)
+	local picker = action_state.get_current_picker(prompt_bufnr)
+	local num_selections = table.getn(picker:get_multi_selection())
+
+	if num_selections > 1 then
+		actions.send_selected_to_qflist(prompt_bufnr)
+		actions.open_qflist(prompt_bufnr)
+	else
+		actions.file_edit(prompt_bufnr)
+	end
+end
+
 require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<cr>"] = fzf_multi_select,
+			},
+			n = {
+				["<cr>"] = fzf_multi_select,
+			},
+		},
+	},
 	extensions = {
 		fzf = {
 			fuzzy = true, -- false will only do exact matching
