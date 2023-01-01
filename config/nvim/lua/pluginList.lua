3002b2bbdc5a233a0144faf310704e6c9cc94518
Alex Manning <alex41290@gmail.com>
2022-12-31 22:33:09 -0500

    add lazy home; remove maybe superfluous osc; use terminal git for lazy

--- config/nvim/lua/pluginList.lua
+++ config/nvim/lua/pluginList.lua
@@ -367,23 +367,6 @@ local plugins = {
 	{
 		"ojroques/nvim-osc52",
 		dependencies = { "folke/which-key.nvim" },
-		config = function()
-			require("osc52").setup()
-
-			local function copy(lines, _)
-				require("osc52").copy(table.concat(lines, "\n"))
-			end
-
-			local function paste()
-				return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-			end
-
-			vim.g.clipboard = {
-				name = "osc52",
-				copy = { ["+"] = copy, ["*"] = copy },
-				paste = { ["+"] = paste, ["*"] = paste },
-			}
-		end,
 	},
 	{
 		"stevearc/dressing.nvim",
@@ -574,5 +557,8 @@ require("lazy").setup(plugins, {
 	install = {
 		-- is there a good way to abstract this?
 		colorscheme = { "duskfox" },
+	},
+	diff = {
+		cmd = "terminal_git",
 	},
 })
