5de92d7c63778f7ea45443d8149a6931e3489de0
Alex Manning <alex41290@gmail.com>
2023-04-28 09:28:30 -0400

    move coffeelint to null; detect bazel for gopls; format

--- config/nvim/lua/plugins/snip.lua
+++ config/nvim/lua/plugins/snip.lua
@@ -1,58 +1,58 @@
 local ls = require("luasnip")
 
 ls.config.set_config({
-  history = true,
-  region_check_events = "InsertEnter",
-  delete_check_events = "TextChanged,InsertLeave",
+	history = true,
+	region_check_events = "InsertEnter",
+	delete_check_events = "TextChanged,InsertLeave",
 })
 
 ls.add_snippets("bzl", {
-  ls.parser.parse_snippet(
-    { trig = "npm_install" },
-    [[npm_install(
+	ls.parser.parse_snippet(
+		{ trig = "npm_install" },
+		[[npm_install(
     name = "$1",
     version = "$2",
     sha256 = "$3",
 )]]
-  ),
-  ls.parser.parse_snippet(
-    { trig = "npm_type_install" },
-    [[npm_install(
+	),
+	ls.parser.parse_snippet(
+		{ trig = "npm_type_install" },
+		[[npm_install(
     name = "$1",
     version = "$2",
     sha256 = "$3",
     type_version = "$4",
     type_sha256 = "$5",
 )]]
-  ),
+	),
 })
 
 ls.add_snippets("gleam", {
-  ls.parser.parse_snippet(
-    { trig = "pub fn" },
-    [[pub fn $1($2) -> $3 {
+	ls.parser.parse_snippet(
+		{ trig = "pub fn" },
+		[[pub fn $1($2) -> $3 {
   $4
 }]]
-  ),
-  ls.parser.parse_snippet(
-    { trig = "fn" },
-    [[fn $1($2) -> $3 {
+	),
+	ls.parser.parse_snippet(
+		{ trig = "fn" },
+		[[fn $1($2) -> $3 {
   $4
 }]]
-  ),
-  ls.parser.parse_snippet(
-    { trig = "pub type" },
-    [[pub type $1 {
+	),
+	ls.parser.parse_snippet(
+		{ trig = "pub type" },
+		[[pub type $1 {
   $2
 }]]
-  ),
-  ls.parser.parse_snippet(
-    { trig = "type" },
-    [[type $1 {
+	),
+	ls.parser.parse_snippet(
+		{ trig = "type" },
+		[[type $1 {
   $2
 }]]
-  ),
-  ls.parser.parse_snippet({ trig = "im" }, [[import $1]]),
+	),
+	ls.parser.parse_snippet({ trig = "im" }, [[import $1]]),
 })
 
 require("luasnip.loaders.from_vscode").lazy_load()
