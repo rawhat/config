048c192220bf026edd3408c8d64a0bf939c83462
Alex Manning <alex41290@gmail.com>
2023-07-27 13:10:38 -0400

    some fish stuff

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,15 +5,15 @@ set -x OMF_PATH "$HOME/.local/share/omf"
 source $OMF_PATH/init.fish
 
 function ls
-  exa $argv
+    exa $argv
 end
 
 function la
-  exa -la $argv
+    exa -la $argv
 end
 
 function code
-  "/c/Program Files/Microsoft VS Code/Code.exe" $argv
+    "/c/Program Files/Microsoft VS Code/Code.exe" $argv
 end
 
 fish_add_path $HOME/.bin
@@ -22,17 +22,18 @@ fish_add_path $HOME/.config/yarn/global/node_modules/.bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.yarn/bin
 fish_add_path $HOME/bin
+fish_add_path /usr/bin/vendor_perl
 
 source ~/.asdf/asdf.fish
 
 set NVIM_BIN (which nvim)
 
 function nvim
-  TERM=wezterm $NVIM_BIN $argv
+    TERM=wezterm $NVIM_BIN $argv
 end
 
-set -x BAT_THEME ""ansi""
-set -x AWS_REGION "us-east-1"
+set -x BAT_THEME ansi
+set -x AWS_REGION us-east-1
 set -x EDITOR (which nvim)
 set -x SUDO_EDITOR (which nvim)
 
