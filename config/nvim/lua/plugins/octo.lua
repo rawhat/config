57b828d4ec36306f9e33681d9f8342c1d7009f01
Alex Manning <alex41290@gmail.com>
2024-01-18 20:49:02 -0500

    only load octo in vistar directory

--- config/nvim/lua/plugins/octo.lua
+++ config/nvim/lua/plugins/octo.lua
@@ -5,6 +5,10 @@ return {
 		"nvim-telescope/telescope.nvim",
 		"nvim-tree/nvim-web-devicons",
 	},
+	cond = function()
+		local cwd = require("utils").cwd()
+		return string.find(cwd, "vistar") ~= nil
+	end,
 	config = function()
 		require("octo").setup()
 	end,
