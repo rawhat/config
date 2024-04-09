90c96bde0f19aed2f26f3215185df2fe0cae7185
Alex Manning <alex41290@gmail.com>
2024-04-09 17:18:53 -0400

    use new blame colors field + add shuffle util

--- config/nvim/lua/plugins/blame.lua
+++ config/nvim/lua/plugins/blame.lua
@@ -4,6 +4,17 @@ return {
 		keys = {
 			{ "<leader>gb", desc = "Toggle git blame", "<cmd>ToggleBlame window<cr>" },
 		},
-		opts = {},
+		config = function()
+			-- TODO:  this is not scoped to theme
+			local scheme = require("kanagawa.colors").setup({ theme = "wave" })
+			local theme_colors = {}
+			for _, value in ipairs(scheme.palette) do
+				table.insert(theme_colors, value)
+			end
+			local colors = require("utils").shuffle(theme_colors)
+			require("blame").setup({
+				colors = colors,
+			})
+		end,
 	},
 }
--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -92,4 +92,16 @@ M.swap_windows = function()
 	vim.api.nvim_set_current_win(win_to_swap)
 end
 
+M.shuffle = function(t)
+	local tbl = {}
+	for i = 1, #t do
+		tbl[i] = t[i]
+	end
+	for i = #tbl, 2, -1 do
+		local j = math.random(i)
+		tbl[i], tbl[j] = tbl[j], tbl[i]
+	end
+	return tbl
+end
+
 return M
