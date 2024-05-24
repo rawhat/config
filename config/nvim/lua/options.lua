6ac86dee8176338cfb2a687c3b98da4e4e0491f8
Alex Manning <alex41290@gmail.com>
2024-05-23 22:52:50 -0400

    update/comment options; ultimate-autopairs -> nvim-autopairs

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -5,51 +5,86 @@ local options = vim.api.nvim_create_augroup("Options", { clear = true })
 -- disable mouse
 vim.cmd("set mouse=")
 
+-- don't display the mode; status bar handles that
 vim.opt.showmode = false
 
+-- offset top/bottom of scroll by 6 rows
 vim.opt.scrolloff = 6
 
+-- highlight searches
 vim.opt.hlsearch = true
+-- start searching while typing
 vim.opt.incsearch = true
+-- briefly jump to matching bracket/paren/etc when placed
 vim.opt.showmatch = true
 
+-- don't consider case when searching
 vim.opt.ignorecase = true
+-- match case when upper is provided
 vim.opt.smartcase = true
 
+-- "smart" indent
 vim.opt.smartindent = true
+-- indent wrapped lines to match previous line
 vim.opt.breakindent = true
 
+-- extend lines past screen instead of wrapping
 vim.opt.wrap = false
 
+-- use file for undo history
 vim.opt.undofile = true
 
--- split options
+-- prefer splitting windows right
 vim.opt.splitright = true
+-- prefer splitting windows below
 vim.opt.splitbelow = true
 
+-- hide concealed characters
 vim.opt.conceallevel = 2
 
+-- display menu even if only one entry
+-- don't automatically select an entry, require actual selection
 vim.opt.completeopt = { "menuone", "noselect" }
 
+-- try these
+vim.opt.jumpoptions = { "stack", "view" }
+-- embed languages in vim strings
+vim.g.vimsyn_embed = "alpPrj"
+-- display small split for substitute-like command changes
+vim.opt.inccommand = "split"
+-- display this character when line wraps (probably not gonna happen
+-- w/ wrap = false?)
+vim.g.showbreak = "↪"
+
+-- make tabs spaces again
 vim.opt.expandtab = true
+-- 4 spaces is too big
 vim.opt.tabstop = 2
+-- always 2
 vim.opt.shiftwidth = 2
+-- yep, should be 2
 vim.opt.softtabstop = 2
 
+-- not sure if this is needed with noice
 vim.opt.wildmenu = true
 vim.opt.wildmode = "longest:full,full"
 
+-- can display 256 colors (maybe not needed)
 vim.g.t_Co = "256"
 
+-- display line numbers
 vim.opt.number = true
+-- display _relative_ line numbers
 vim.opt.relativenumber = true
 
+-- display different highlight for line cursor is on
 vim.opt.cursorline = true
 
 -- don't really use these, but treesitter parsing for folds
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
 
+-- probably don't need this, but set filetype to bazel for... bazel files
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = { "BUILD", "*.bzl", "WORKSPACE", "BUILD.bazel" },
 	command = "setf bzl",
@@ -59,20 +94,28 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 -- for toggle term, reuse the same term
 vim.opt.hidden = true
 
+-- write to swap file after this many milliseconds of inaction
 vim.opt.updatetime = 50
+-- wait for key combinations for this many milliseconds
 vim.opt.timeoutlen = 300
 
+-- don't display when file is written
+-- don't display search count (noice)
+-- don't display messages for completion stuff? idk
 vim.opt.shortmess:append("WcC")
 
 -- always show column to left of numbers for lsp/git symbols
 -- vim.wo.signcolumn = "auto:1-2"
 vim.wo.signcolumn = "yes"
 
+-- leader is space
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- write file and overwrite old backup? i think
 vim.g.backupcopy = "yes"
 
+-- gimme the full gamut
 vim.opt.termguicolors = true
 
 -- allow cursor to go one char past the EOL
@@ -85,17 +128,15 @@ vim.g.python_recommended_style = 0
 vim.g.ackprg = "rg --vimgrep --no-heading --smart-case"
 vim.cmd([[cnoreabbrev rg Ack]])
 
-vim.g.mix_format_on_save = 1
-
-vim.g.node_client_debug = 1
-
-vim.g.termsync = 0
-
+-- show status line only once
 vim.o.laststatus = 3
+-- hide command line
 vim.opt.cmdheight = 0
 
+-- just draw the terminal as fast as possible
 vim.g.notermsync = true
 
+-- don't move cursor when splitting
 if vim.fn.exists("&splitkeep") ~= 0 then
 	vim.opt.splitkeep = "screen"
 end
@@ -117,6 +158,7 @@ vim.g.markdown_fenced_languages = {
 	"typescriptreact",
 }
 
+-- try to avoid slowness of looking up clipboard
 vim.opt.clipboard = "unnamedplus"
 
 -- make 0 go to first word in line instead of start of line...
@@ -127,7 +169,5 @@ vim.api.nvim_exec(
   ]],
 	false
 )
-
-vim.g.ts_highlight_lua = true
 
 return M
--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -15,6 +15,9 @@ return {
 		local cmp = require("cmp")
 		local lspkind = require("lspkind")
 		local luasnip = require("luasnip")
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 		luasnip.config.setup({})
 
