b84e9950311fcdb46eaf47b0a1c3c9f4f56dafe7
Alex Manning <alex41290@gmail.com>
2024-06-21 09:10:24 -0400

    use gitsigns blame

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -14,10 +14,20 @@ return {
 				end)
 			end,
 		},
+		{ "<leader>gb", desc = "Open git blame", "<cmd>Gitsigns blame<cr>" },
 	},
 	config = function()
 		require("gitsigns").setup({
 			trouble = true,
+		})
+
+		vim.api.nvim_create_autocmd({ "FileType" }, {
+			pattern = { "gitsigns.blame" },
+			callback = function()
+				vim.keymap.set("n", "q", function()
+					vim.api.nvim_buf_delete(0, {})
+				end, { buffer = true })
+			end,
 		})
 	end,
 }
