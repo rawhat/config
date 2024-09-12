db4e0335402df63ab4dd533fbb088ee78951408f
Alex Manning <alex41290@gmail.com>
2024-09-12 09:17:53 -0400

    `npm_install` function

--- config/nvim/lua/cmds.lua
+++ config/nvim/lua/cmds.lua
@@ -24,7 +24,7 @@ vim.api.nvim_create_autocmd({ "ModeChanged" }, {
 })
 
 -- TODO:  do i care about supporting a custom path?
-vim.api.nvim_create_user_command("Decaffeinate", function(args)
+vim.api.nvim_create_user_command("Decaffeinate", function()
 	local cmd = vim.o.shell .. " -l bazel run @decaffeinate//:run -- " .. vim.fn.expand("%:p")
 	require("terminal").run({
 		"bazel",
@@ -36,6 +36,42 @@ vim.api.nvim_create_user_command("Decaffeinate", function(args)
 		layout = { open_cmd = "botright vertical new" },
 	})
 end, {})
+
+local npm_install = function(args)
+	if type(args) == "string" then
+		args = vim.trim(args)
+	end
+	local package_info = vim.system({ "npm", "show", "--json", args }, { text = true }):wait(1000)
+	local parsed = vim.json.decode(package_info.stdout)
+
+	local tarball = vim.system({ "curl", "-s", parsed.dist.tarball }):wait()
+	local sha_cmd = vim.system({ "sha256sum" }, { stdin = tarball.stdout, text = true }):wait()
+	local head_cmd = vim.system({ "head", "-c", "64" }, { stdin = sha_cmd.stdout, text = true }):wait()
+	local sha = head_cmd.stdout
+
+	if sha == "" then
+		vim.notify("Failed to obtain sha for " .. args, vim.log.levels.ERROR)
+	end
+
+	local install = 'npm_install(\n\tname = "'
+		.. parsed.name
+		.. '",\n\tversion = "'
+		.. parsed.version
+		.. '",\n\tsha256 = "'
+		.. sha
+		.. '"\n)'
+
+	return install
+end
+
+vim.api.nvim_create_user_command("NpmInstall", function(command)
+	local args = command.args
+	if type(args) == "string" then
+		args = vim.trim(args)
+	end
+	local install = npm_install(args)
+	vim.fn.setreg("+", install)
+end, { nargs = "*" })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
 	pattern = { "*.gleam" },
