9bc3c66aba2e54e3e2580920248eaf9e2d0670b3
Alex Manning <alex41290@gmail.com>
2024-10-15 09:23:36 -0400

    clean up blink config

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -3,39 +3,21 @@ return {
 	lazy = false,
 	build = "cargo build --release",
 	opts = {
-		keymap = {
-			select_prev = { "<C-p>" },
-			select_next = { "<C-n>" },
-		},
 		accept = {
 			auto_brackets = {
 				enabled = true,
 			},
 		},
-		--[[ trigger = {
-			signature_help = {
-				enabled = true,
-			},
-		}, ]]
 		highlight = {
-			use_npm_cmp_as_default = true,
+			use_nvim_cmp_as_default = true,
 		},
-		sources = {
-			providers = {
-				{
-					{ "blink.cmp.sources.lsp" },
-					{ "blink.cmp.sources.path" },
-				},
-				{
-					{ "blink.cmp.sources.buffer" },
-				},
+		windows = {
+			autocomplete = {
+				draw = "minimal",
+			},
+			documentation = {
+				auto_show = true,
 			},
 		},
-		--[[ fuzzy = {
-			prebuiltBinaryies = {
-				download = true,
-				forceVersion = "main",
-			},
-		}, ]]
 	},
 }
