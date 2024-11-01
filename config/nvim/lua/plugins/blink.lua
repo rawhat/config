7bc0706c0a97879d6c61ed6ad91f3d9e945a990a
Alex Manning <alex41290@gmail.com>
2024-11-01 12:06:25 -0400

    lazydev blink integration

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -12,6 +12,15 @@ return {
 			use_nvim_cmp_as_default = true,
 		},
 		keymap = "super-tab",
+		sources = {
+			completion = {
+				enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+			},
+			providers = {
+				lsp = { fallback_for = { "lazydev" } },
+				lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+			},
+		},
 		windows = {
 			autocomplete = {
 				draw = "minimal",
