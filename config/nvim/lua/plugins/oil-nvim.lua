c3b0c2a678c85c0dbd6f59becbc28ba7938c571e
Alex Manning <alex41290@gmail.com>
2025-01-31 09:14:35 -0500

    csv ft; oil split keybind

--- config/nvim/lua/plugins/oil-nvim.lua
+++ config/nvim/lua/plugins/oil-nvim.lua
@@ -18,7 +18,7 @@ return {
 				q = "actions.close",
 				["<Esc>"] = "actions.close",
 				["<C-v>"] = "actions.select_vsplit",
-				["<C-x>"] = "actions.select_split",
+				["<C-s>"] = "actions.select_split",
 				["<C-h>"] = false,
 				["<C-t>"] = false,
 				["<C-q>"] = "actions.send_to_qflist",
