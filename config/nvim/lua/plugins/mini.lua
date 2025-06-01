eec1d965ca86c3a48e3e46384e8b86e183c07bf8
Alex Manning <alex41290@gmail.com>
2025-06-01 18:21:56 -0400

    moveline -> mini.move

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -1,4 +1,21 @@
 return {
 	{ "echasnovski/mini.align", version = false, opts = {} },
 	{ "echasnovski/mini.ai", version = false, opts = {} },
+	{
+		"echasnovski/mini.move",
+		version = false,
+		opts = {
+			mappings = {
+				left = "<C-h>",
+				right = "<C-l>",
+				down = "<C-j>",
+				up = "<C-k>",
+
+				line_left = "<C-h>",
+				line_right = "<C-l>",
+				line_down = "<C-j>",
+				line_up = "<C-k>",
+			},
+		},
+	},
 }
