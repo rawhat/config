759e31e91348d956f61dfd961345497b1d3e25ce
Alex Manning <alex41290@gmail.com>
2025-06-12 18:15:09 -0400

    treesitter-context opts

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter-context",
+		{ "nvim-treesitter/nvim-treesitter-context", opts = {} },
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		"windwp/nvim-ts-autotag",
 		"Rrethy/nvim-treesitter-endwise",
