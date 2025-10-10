e612407c92a9970d11f8b19668e3f8202a66f0ad
Alex Manning <alex41290@gmail.com>
2025-10-10 15:12:17 -0400

    trying ix?

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,64 +1,65 @@
-local opts = {
-	"saghen/blink.cmp",
-	lazy = false,
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-	},
-	opts = {
-		keymap = {
-			preset = "super-tab",
-		},
-		cmdline = {
-			completion = {
-				menu = {
-					auto_show = function(ctx)
-						return vim.fn.getcmdtype() == ":" or vim.fn.getcmdtype() == "@"
-					end,
-				},
-			},
-			keymap = {
-				["<Tab>"] = { "show", "accept" },
-				-- preset = "inherit",
-			},
-		},
-		completion = {
-			accept = {
-				resolve_timeout_ms = 10000,
-			},
-			keyword = {
-				range = "full",
-			},
-			trigger = {
-				show_in_snippet = false,
-				show_on_backspace_in_keyword = true,
-			},
-			documentation = {
-				auto_show = true,
-			},
-		},
-		signature = {
-			enabled = true,
-			trigger = {
-				show_on_insert = true,
-			},
-		},
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-			per_filetype = {
-				lua = { inherit_defaults = true, "lazydev" },
-			},
-			providers = {
-				lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", fallbacks = { "lsp" } },
-				lsp = { fallbacks = {} },
-			},
-		},
-	},
-}
-
-if jit.os:lower() ~= "windows" then
-	opts.build = "cargo build --release"
-else
-	opts.version = "1.*"
-end
-
-return opts
+return {}
+-- local opts = {
+-- 	"saghen/blink.cmp",
+-- 	lazy = false,
+-- 	dependencies = {
+-- 		"rafamadriz/friendly-snippets",
+-- 	},
+-- 	opts = {
+-- 		keymap = {
+-- 			preset = "super-tab",
+-- 		},
+-- 		cmdline = {
+-- 			completion = {
+-- 				menu = {
+-- 					auto_show = function(ctx)
+-- 						return vim.fn.getcmdtype() == ":" or vim.fn.getcmdtype() == "@"
+-- 					end,
+-- 				},
+-- 			},
+-- 			keymap = {
+-- 				["<Tab>"] = { "show", "accept" },
+-- 				-- preset = "inherit",
+-- 			},
+-- 		},
+-- 		completion = {
+-- 			accept = {
+-- 				resolve_timeout_ms = 10000,
+-- 			},
+-- 			keyword = {
+-- 				range = "full",
+-- 			},
+-- 			trigger = {
+-- 				show_in_snippet = false,
+-- 				show_on_backspace_in_keyword = true,
+-- 			},
+-- 			documentation = {
+-- 				auto_show = true,
+-- 			},
+-- 		},
+-- 		signature = {
+-- 			enabled = true,
+-- 			trigger = {
+-- 				show_on_insert = true,
+-- 			},
+-- 		},
+-- 		sources = {
+-- 			default = { "lsp", "path", "snippets", "buffer" },
+-- 			per_filetype = {
+-- 				lua = { inherit_defaults = true, "lazydev" },
+-- 			},
+-- 			providers = {
+-- 				lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", fallbacks = { "lsp" } },
+-- 				lsp = { fallbacks = {} },
+-- 			},
+-- 		},
+-- 	},
+-- }
+--
+-- if jit.os:lower() ~= "windows" then
+-- 	opts.build = "cargo build --release"
+-- else
+-- 	opts.version = "1.*"
+-- end
+--
+-- return opts
