51b23b03204e126ee0c144cabc95650ac9186f86
Alex Manning <alex41290@gmail.com>
2026-06-05 17:17:14 -0400

    setup workspaces; autostart on workspaces

--- config/hypr/monitors.lua
+++ config/hypr/monitors.lua
@@ -16,3 +16,35 @@ hl.monitor({
 	scale = 1,
 	-- vrr = 2,
 })
+
+hl.workspace_rule({
+	workspace = "1",
+	monitor = "DP-3",
+	default = true,
+	persistent = true,
+})
+
+hl.workspace_rule({
+	workspace = "2",
+	monitor = "DP-3",
+	persistent = true,
+})
+
+hl.workspace_rule({
+	workspace = "3",
+	monitor = "DP-3",
+	persistent = true,
+})
+
+hl.workspace_rule({
+	workspace = "4",
+	monitor = "HDMI-A-1",
+	default = true,
+	persistent = true,
+})
+
+hl.workspace_rule({
+	workspace = "5",
+	monitor = "HDMI-A-1",
+	persistent = true,
+})
